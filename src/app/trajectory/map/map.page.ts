3d8afd530495236a7d386db84e01972b8bfe05ed
Sven Heitmann <s.s.h@uni-muenster.de>
2020-09-30 15:34:40 +0200

    display map on map page using leaflet

--- src/app/trajectory/map/map.page.ts
+++ src/app/trajectory/map/map.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPage implements OnInit {
 
+  private map;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {  }
+
+  ionViewDidEnter () {
+    this.initMap();
+  }
+
+  ionViewDidLeave () {
+    this.map.remove();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [51.9694, 7.5954],
+      zoom: 3
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
   }
 
 }
