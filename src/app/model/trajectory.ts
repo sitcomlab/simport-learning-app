6c1b3fc4acccc6cde7be3cf4ac9d043d16b4280a
Norwin Roosen <git@nroo.de>
2020-11-07 16:17:33 +0100

    implement Trajectory model as class
    for consistent (computed) data access & modification

--- src/app/model/trajectory.ts
+++ src/app/model/trajectory.ts
@@ -1,3 +1,5 @@
+import * as moment from 'moment'
+
 export enum TrajectoryType {
   EXAMPLE   = 'example',
   IMPORT    = 'import',
@@ -16,4 +18,52 @@ export interface TrajectoryData {
   timestamps: Date[],
 }
 
-export interface Trajectory extends TrajectoryMeta, TrajectoryData { }
+export class Trajectory implements TrajectoryMeta, TrajectoryData {
+  constructor(
+    private meta: TrajectoryMeta,
+    private data?: TrajectoryData,
+  ) {
+    if (data?.coordinates.length !== data?.timestamps.length)
+      throw new Error('data corruption; coordinates & timestamps don\'t have equal length')
+  }
+
+  // implement TrajectoryMeta interface
+  get id() { return this.meta.id }
+  get type() { return this.meta.type }
+  get placename() { return this.meta.placename }
+  get durationDays() {
+    // try to compute durationDays for precision
+    const ts = this.data?.timestamps
+    if (ts?.length) {
+      const t1 = moment(ts[0])
+      const t2 = moment(ts[ts.length - 1])
+      return t1.diff(t2, 'days')
+    }
+    // fall back to stored value
+    return this.meta.durationDays || 0
+  }
+
+
+  // implement TrajectoryData interface
+  get coordinates() { return this.data?.coordinates || [] }
+  get timestamps() { return this.data?.timestamps || [] }
+
+
+  get durationString() {
+    return moment.duration(this.durationDays, 'days').humanize()
+  }
+
+  get points(): [number, number, Date][] {
+    const points = []
+    const n = this.data?.coordinates.length || 0
+    for (let i = 0; i < n; i++) {
+      points.push([...this.data.coordinates[i], this.data.timestamps[i]])
+    }
+    return points
+  }
+
+  addPoint(latLng: [number, number], time?: Date) {
+    this.coordinates.push(latLng)
+    this.timestamps.push(time || new Date())
+  }
+}
--- src/app/select-trajectory/trajectory-card/trajectory-card.component.ts
+++ src/app/select-trajectory/trajectory-card/trajectory-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core'
-import * as moment from 'moment'
 import { Trajectory } from 'src/app/model/trajectory'
 
 @Component({
@@ -13,18 +12,4 @@ export class TrajectoryCardComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-
-  get duration() {
-    const { durationDays, timestamps: ts } = this.trajectory
-
-    // use hardcoded value if available
-    if (durationDays !== undefined)
-      return moment.duration(durationDays, 'days').humanize()
-
-    // fallback to computing
-    if (!ts || !ts.length) return ''
-    const t1 = moment(ts[0])
-    const t2 = moment(ts[ts.length - 1])
-    return t1.from(t2, true)
-  }
 }
