7012da240830f02d9fafc4912961e7b4555e6b72
Norwin Roosen <git@nroo.de>
2021-02-10 15:22:49 +0100

    refactor to InferenceDefinition-based formatting

--- src/app/trajectory/inferences/inferences.page.ts
+++ src/app/trajectory/inferences/inferences.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Router } from '@angular/router'
+import { AllInferences } from 'src/app/shared-services/inferences/definitions'
 import { InferenceService } from './inference.service'
 
 @Component({
@@ -13,12 +14,24 @@ export class InferencesPage implements OnInit {
   constructor(
     private service: InferenceService,
     private router: Router,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
     const trajId = this.route.snapshot.paramMap.get('trajectoryId')
     this.inferences = this.service.getInferences(trajId)
+  }
+
+  formatInferenceName(inference: Inference): string {
+    const def = AllInferences[inference.name]
+    if (!def) return inference.name
+    return def.name()
+  }
+
+  formatInferenceInfo(inference: Inference): string {
+    const def = AllInferences[inference.name]
+    if (!def) return `unknown inference ${inference.name}`
+    return def.info(inference)
   }
 
   showInferenceOnMap(inference: Inference) {
