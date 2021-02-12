2c7f1104f5075dd65a09a7b98d150843e0ff0d14
gerrit:jdreyer <j.dreyer@fh-muenster.de>
2021-02-12 15:12:55 +0100

    Added start/stop icon to tracking

--- src/app/tracking/tracking.page.ts
+++ src/app/tracking/tracking.page.ts
@@ -13,6 +13,7 @@ import { TrajectoryService } from '../shared-services/trajectory.service'
 })
 export class TrackingPage implements OnInit, OnDestroy {
   @Input() state: string
+  @Input() stateIcon: string
   @Input() notificationsEnabled: boolean
 
   private locationServiceStateSubscription: Subscription
@@ -34,6 +35,7 @@ export class TrackingPage implements OnInit, OnDestroy {
     this.locationServiceStateSubscription = this.locationService.isRunning.subscribe(
       (state) => {
         this.setState(state ? 'Running' : 'Stopped')
+        this.setStateIcon(state)
       }
     )
     this.locationServiceNotificationToggleSubscription = this.locationService.notificationsEnabled.subscribe(
@@ -62,6 +64,12 @@ export class TrackingPage implements OnInit, OnDestroy {
   setState(state: string) {
     this.zone.run(() => {
       this.state = state
+    })
+  }
+
+  setStateIcon(running: boolean) {
+    this.zone.run(() => {
+      this.stateIcon = running ? 'stop-circle' : 'play-circle'
     })
   }
 
