bbd583d6c36323e3d93a282ddd1bf5ffea170c2a
felixerdy <felix.erdmann@uni-muenster.de>
2021-04-08 11:53:29 +0200

    #55: fix tests

--- src/app/debug-window/debug-window.component.spec.ts
+++ src/app/debug-window/debug-window.component.spec.ts
@@ -1,5 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { BackgroundGeolocation } from '@ionic-native/background-geolocation/ngx'
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx'
 import { IonicModule } from '@ionic/angular'
+import { SqliteService } from '../shared-services/db/sqlite.service'
+import { LocationService } from '../shared-services/location.service'
+import { TrajectoryService } from '../shared-services/trajectory.service'
 
 import { DebugWindowComponent } from './debug-window.component'
 
@@ -10,7 +16,14 @@ describe('DebugWindowComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [DebugWindowComponent],
-      imports: [IonicModule.forRoot()],
+      imports: [IonicModule.forRoot(), HttpClientTestingModule],
+      providers: [
+        TrajectoryService,
+        SqliteService,
+        LocationService,
+        BackgroundGeolocation,
+        LocalNotifications,
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(DebugWindowComponent)
