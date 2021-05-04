841ac89f5326f6906a45217c7e1d6b9f2eb2a854
Norwin Roosen <git@nroo.de>
2021-05-04 21:27:47 +0200

    unfuck indentation

--- dev/anonymize_trajectory.ts
+++ dev/anonymize_trajectory.ts
@@ -2,10 +2,10 @@ const USAGE = `ts-node -r tsconfig-paths/register --dir dev anonymize_trajectory
 
 import * as fs from 'fs'
 import haversine from 'haversine-distance'
-import { Trajectory, TrajectoryData } from 'src/app/model/trajectory'
+import { Trajectory } from 'src/app/model/trajectory'
 
 function encodeData(d: any) {
-	const sep = ','
+  const sep = ','
   const result = [
     '"' + ['time','d_seconds','d_meters','speed','accuracy'].join(`"${sep}"`) + '"',
   ]
@@ -15,8 +15,8 @@ function encodeData(d: any) {
     let dT = 0
     if (i > 0) {
       dS = dist(t.coordinates[i], t.coordinates[i-1])
-			dT = d.timestamps[i-1]
-		}
+      dT = d.timestamps[i-1]
+    }
     result.push([t.timestamps[i].toISOString(), dT, dS, t.speed[i], t.accuracy[i]].join(sep))
   }
   return result.join('\r\n')
@@ -38,7 +38,6 @@ function argparse() {
   return { filepath }
 }
 
- 
 async function main() {
   const { filepath } = argparse()
   const content = fs.readFileSync(filepath, { encoding: 'utf-8' })
