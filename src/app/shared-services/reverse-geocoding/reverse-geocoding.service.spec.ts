a4fb58a1013097a828de1dde2649c8f3d2a13f7b
gerrit:jdreyer <j.dreyer@fh-muenster.de>
2021-11-08 10:24:36 +0100

    #99: fix test

--- src/app/shared-services/reverse-geocoding/reverse-geocoding.service.spec.ts
+++ src/app/shared-services/reverse-geocoding/reverse-geocoding.service.spec.ts
@@ -1,11 +1,16 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
+import { SqliteService } from '../db/sqlite.service'
 import { ReverseGeocodingService } from './reverse-geocoding.service'
 
 describe('ReverseGeocodingService', () => {
   let service: ReverseGeocodingService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ providers: [] })
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [SqliteService],
+    })
     service = TestBed.inject(ReverseGeocodingService)
   })
 
