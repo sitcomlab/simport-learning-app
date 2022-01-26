d28b91750630acbfa628d1dfff91531b393f153f
Felix Erdmann <hello@felixerdmann.com>
2022-01-26 12:16:32 +0100

    135: trajectory filter l10n

--- src/app/trajectory/inference-filter/inference-filter.module.ts
+++ src/app/trajectory/inference-filter/inference-filter.module.ts
@@ -6,9 +6,16 @@ import { IonicModule } from '@ionic/angular'
 
 import { InferenceFilterComponent } from './inference-filter.component'
 import { SharedUiModule } from 'src/app/shared-ui/shared-ui.module'
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, SharedUiModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SharedUiModule,
+    TranslateModule,
+  ],
   declarations: [InferenceFilterComponent],
 })
 export class InferenceFilterModule {}
