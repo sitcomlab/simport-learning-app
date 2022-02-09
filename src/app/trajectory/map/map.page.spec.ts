b69e90113fbb8e3f317cfe6f55c8d4c36a8788a9
Felix Erdmann <hello@felixerdmann.com>
2022-02-09 13:46:08 +0100

    #140: fix tests

--- src/app/trajectory/map/map.page.spec.ts
+++ src/app/trajectory/map/map.page.spec.ts
@@ -3,7 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { LeafletModule } from '@asymmetrik/ngx-leaflet'
 import { BackgroundGeolocation } from '@ionic-native/background-geolocation/ngx'
-import { IonicModule } from '@ionic/angular'
+import { IonicModule, IonRouterOutlet } from '@ionic/angular'
+import { FeatureFlagService } from 'src/app/shared-services/feature-flag/feature-flag.service'
 import { SqliteService } from '../../shared-services/db/sqlite.service'
 import { LocationService } from '../../shared-services/location/location.service'
 import { TrajectoryService } from '../../shared-services/trajectory/trajectory.service'
@@ -27,6 +28,13 @@ describe('MapPage', () => {
         TrajectoryService,
         BackgroundGeolocation,
         SqliteService,
+        {
+          // use empty IonRouterOutlet, since actually providing IonRouterOutlet
+          // creates a conflict with RouterTestingModule and this is sufficent for running tests.
+          provide: IonRouterOutlet,
+          useValue: { nativeEl: '' },
+        },
+        FeatureFlagService,
       ],
     }).compileComponents()
 
