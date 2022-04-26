91e86374d010f8029b67540871ae74e786dfb7f9
Felix Erdmann <hello@felixerdmann.com>
2022-04-26 12:33:27 +0200

    #179: export logfile with share dialog

--- src/app/shared-services/logfile/logfile.service.ts
+++ src/app/shared-services/logfile/logfile.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core'
-import { Plugins } from '@capacitor/core'
+import {
+  FilesystemDirectory,
+  FilesystemEncoding,
+  Plugins,
+} from '@capacitor/core'
 import { Platform } from '@ionic/angular'
+import { TranslateService } from '@ngx-translate/core'
 import { LogEvent } from 'src/app/model/log-event'
 import { SqliteService } from '../db/sqlite.service'
 import { TrajectoryService } from '../trajectory/trajectory.service'
 import { LogEventLevel, LogEventScope, LogEventType } from './types'
-const { App } = Plugins
+const { App, Filesystem, Share } = Plugins
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +19,8 @@ export class LogfileService {
   constructor(
     private platform: Platform,
     private dbService: SqliteService,
-    private trajectoryService: TrajectoryService
+    private trajectoryService: TrajectoryService,
+    private translateService: TranslateService
   ) {
     this.platform.ready().then(() => {
       this.log('App started', LogEventScope.app, LogEventType.start)
@@ -53,7 +59,39 @@ export class LogfileService {
     })
   }
 
-  exportLog(): Promise<LogEvent[]> {
-    return this.dbService.getLogs()
+  async exportLog() {
+    try {
+      const logs = await this.dbService.getLogs()
+      const csvHeader = `${Object.getOwnPropertyNames(
+        new (LogEvent as any)()
+      ).join()}\n`
+
+      const fileData =
+        csvHeader + logs.map((l) => Object.values(l).join()).join('\n')
+
+      const fileResult = await Filesystem.writeFile({
+        data: fileData,
+        path: `SIMPORT_log_${
+          new Date().toISOString().replace(/:/g, '-').split('.')[0]
+        }.csv`,
+        directory: FilesystemDirectory.ExternalStorage,
+        encoding: FilesystemEncoding.UTF8,
+      })
+
+      if (this.platform.is('android')) {
+        await Share.requestPermissions()
+      }
+
+      Share.share({
+        title: 'SIMPORT Log',
+        url: fileResult.uri,
+      })
+    } catch (e) {
+      const errorMessage = this.translateService.instant(
+        'log.exportFileErrorTitle',
+        { value: e.message }
+      )
+      throw new Error(errorMessage)
+    }
   }
 }
