a4bad69a690f053f73a1c0cee5234bfcff4235e2
gerrit:jdreyer <j.dreyer@fh-muenster.de>
2022-07-27 15:09:24 +0200

    #219: adjust file-write directory for Android 10 and above + refactor

--- src/app/diary/diary.page.ts
+++ src/app/diary/diary.page.ts
@@ -1,15 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
-import { Platform, ToastController } from '@ionic/angular'
-import { TranslateService } from '@ngx-translate/core'
+import { ToastController } from '@ionic/angular'
 import { DiaryEntry } from '../model/diary-entry'
 import { DiaryService } from '../shared-services/diary/diary.service'
-import { LogfileService } from '../shared-services/logfile/logfile.service'
-
-import { FilesystemDirectory, Plugins } from '@capacitor/core'
-const { Filesystem, Share } = Plugins
-
-import JSZip from 'jszip'
 
 @Component({
   selector: 'app-diary',
@@ -22,10 +15,7 @@ export class DiaryPage implements OnInit {
   constructor(
     private router: Router,
     private diaryService: DiaryService,
-    private toastController: ToastController,
-    private translateService: TranslateService,
-    private logfileService: LogfileService,
-    private platform: Platform
+    private toastController: ToastController
   ) {}
 
   ngOnInit() {}
@@ -36,38 +26,9 @@ export class DiaryPage implements OnInit {
     )
   }
 
-  /**
-   * @description Generates a .zip File containing the latest diary and logfile data and exports it via the Capacitor `Share` plugin
-   */
   async exportDiary() {
     try {
-      const diaryData = await this.diaryService.exportDiary()
-      const logfileData = await this.logfileService.exportLog()
-
-      const timestamp = new Date()
-        .toISOString()
-        .replace(/:/g, '-')
-        .split('.')[0]
-
-      const zip = new JSZip()
-      zip.file(`diary_${timestamp}.csv`, diaryData)
-      zip.file(`log_${timestamp}.csv`, logfileData)
-
-      const data = await zip.generateAsync({ type: 'base64' })
-      const fileResult = await Filesystem.writeFile({
-        data,
-        path: `SIMPORT_export_${timestamp}.zip`,
-        directory: FilesystemDirectory.ExternalStorage,
-      })
-
-      if (this.platform.is('android')) {
-        await Share.requestPermissions()
-      }
-
-      Share.share({
-        title: this.translateService.instant('diary.exportFileName'),
-        url: fileResult.uri,
-      })
+      await this.diaryService.exportDiary()
     } catch (e) {
       const toast = await this.toastController.create({
         message: e.message,
