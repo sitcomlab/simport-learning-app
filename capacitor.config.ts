e57b4e0302392fb99fe5439544ffba4a170a475a
Felix Erdmann <hello@felixerdmann.com>
2023-01-18 11:33:12 +0100

    feat: replace `capacitor.config.json` with `.ts`

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+ appId: 'de.ifgi.simport.learning',
+  appName: 'simport-learning',
+  bundledWebRuntime: false,
+  webDir: 'www',
+}
+
+export default config
